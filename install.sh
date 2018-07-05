45b89dbcfa8285ae7182cfc2d79182ce469a94c4
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-05 22:30:44 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt install ddccontrol i2c-tools
+# install dependencies
+sudo apt install git build-essentials ddccontrol i2c-tools
+
+# add i2c udev rules
 sudo groupadd --system i2c-dev
 sudo cp i2c-dev.rules /etc/udev/rules.d/
 sudo usermod -a -G i2c-dev $USERNAME
 # sudo /bin/sh -c 'echo i2c-dev >> /etc/modules'
 
-# TODO: Install acdcontrol and add udev rules
+# Install acdcontrol and add hid udev rules
+git clone https://github.com/warvariuc/acdcontrol.git
+cd acdcontrol && make
+sudo cp acdcontrol /usr/bin
+sudo groupadd --system hid-dev
+sudo cp hid-dev.rules /etc/udev/rules.d/
+sudo usermod -a -G hid-dev $USERNAME
 
 # TODO: install bright
