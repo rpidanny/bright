d8b79f6b09e7c1c29e8cb363b73bef4bfdc930b8
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-05 23:02:30 +0200

    cleanup

--- bright.sh
+++ bright.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scale=0.01899
-scaleeight=0.04835
+SCALEP=0.01899 # scale for 0 - 100
+SCALEE=0.04835 # scale for 0 - 255
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -32,8 +32,8 @@ fi
 echo_green "Listening for brightness change..."
 while inotifywait -q -e modify /sys/class/backlight/intel_backlight/brightness -o /dev/null; do
   brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
-  percent=$(echo $brightness $scale | awk '{printf "%d\n",$1*$2}')
-  eightbit=$(echo $brightness $scaleeight | awk '{printf "%d\n",$1*$2}')
+  percent=$(echo $brightness $SCALEP | awk '{printf "%d\n",$1*$2}')
+  eightbit=$(echo $brightness $SCALEE | awk '{printf "%d\n",$1*$2}')
 
   for a in $HID_DISPLAYS; do
     acdcontrol -s $a $eightbit
