3ad80a51e95001394098138b155b4de85c712166
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-13 10:14:20 +0200

    i2c displays support quick fix

--- bright.sh
+++ bright.sh
@@ -11,23 +11,21 @@ echo_green () {
 }
 
 # detect HID displays
+echo_green "Looking for HID Displays"
+HID_DISPLAYS=$(acdcontrol -s -d /dev/usb/hiddev* | tr --delete ':' | awk '{print $1} ')
+echo $HID_DISPLAYS
+echo ""
+
+# Current implementation: only look for i2c displays if hid displays not found
+# TODO: scan independent to hid display and filter out devices already included in hid
+# detect i2c displays supporting VESA
 if [ -z ${HID_DISPLAYS} ] ;
 then
-  echo_green "Looking for HID Displays"
-  HID_DISPLAYS=$(acdcontrol -s -d /dev/usb/hiddev* | tr --delete ':' | awk '{print $1} ')
-  echo $HID_DISPLAYS
+  echo_green "Looking for i2c Displays"
+  I2C_DISPLAYS=$(ddccontrol -p 2> /dev/null | grep "Reading EDID and initializing DDC/CI at bus" | tr --delete '...' | awk '{print $8}')
+  echo $I2C_DISPLAYS
   echo ""
 fi
-
-# TODO: Filter out devices already included in hid
-# detect i2c displays supporting VESA
-# if [ -z ${GENERIC_DISPLAYS} ] ;
-# then
-#   echo_green "Looking for i2c Displays"
-#   GENERIC_DISPLAYS=$(ddccontrol -p 2> /dev/null | grep "Reading EDID and initializing DDC/CI at bus" | tr --delete '...' | awk '{print $8}')
-#   echo $GENERIC_DISPLAYS
-#   echo ""
-# fi
 
 # listen for brigntness change
 echo_green "Listening for brightness change..."
@@ -39,7 +37,7 @@ while inotifywait -q -e modify /sys/class/backlight/intel_backlight/brightness -
   for a in $HID_DISPLAYS; do
     acdcontrol -s $a $eightbit
   done
-  # for d in ${GENERIC_DISPLAYS}; do
-  #   ddccontrol ${d} -f -r 0x10 -w $percent &> /dev/null
-  # done
+  for d in ${I2C_DISPLAYS}; do
+    ddccontrol ${d} -f -r 0x10 -w $percent &> /dev/null
+  done
 done
