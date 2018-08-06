0275deb93f7d31967e69ddbee52d914d3b64b557
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-06 14:27:57 +0200

    removed unnecessary udev rules

--- install.sh
+++ install.sh
@@ -8,31 +8,41 @@ success () {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-# Install ddccontrol and add i2c udev rules
-info "Installing ddccontrol"
-sudo apt install i2c-tools ddccontrol ddccontrol-db
-info "Adding i2c udev rule"
-sudo groupadd --system i2c-dev
-sudo cp i2c-dev.rules /etc/udev/rules.d/
-sudo usermod -a -G i2c-dev $USERNAME
-# sudo /bin/sh -c 'echo i2c-dev >> /etc/modules'
+# # Install ddccontrol and add i2c udev rules
+# info "Installing ddccontrol"
+# sudo apt install i2c-tools ddccontrol ddccontrol-db
+# info "Adding i2c udev rule"
+# sudo groupadd --system i2c-dev
+# sudo cp i2c-dev.rules /etc/udev/rules.d/
+# sudo usermod -a -G i2c-dev $USERNAME
+# # sudo /bin/sh -c 'echo i2c-dev >> /etc/modules'
 
-# Install acdcontrol and add hid udev rules
+# # Install acdcontrol and add hid udev rules
+# info "Installing acdcontrol"
+# git clone https://github.com/warvariuc/acdcontrol.git
+# cd acdcontrol && make
+# sudo cp acdcontrol /usr/bin
+# info "Adding hid udev rule"
+# sudo groupadd --system hid-dev
+# sudo cp hid-dev.rules /etc/udev/rules.d/
+# sudo usermod -a -G hid-dev $USERNAME
+
+# install dependencies
+info "Installing ddccontrol"
+sudo apt install -y i2c-tools ddccontrol ddccontrol-db
+
 info "Installing acdcontrol"
 git clone https://github.com/warvariuc/acdcontrol.git
 cd acdcontrol && make
 sudo cp acdcontrol /usr/bin
-info "Adding hid udev rule"
-sudo groupadd --system hid-dev
-sudo cp hid-dev.rules /etc/udev/rules.d/
-sudo usermod -a -G hid-dev $USERNAME
 
-# TODO: install bright
-info "Installing bright"
-sudo apt install -f inotify-tools
+info "Installing inotify-tools"
+sudo apt install -y inotify-tools
+
+# install bright
 sudo mkdir -p /etc/brightSync
 git clone https://github.com/rpidanny/bright.git /etc/brightSync
-sudo cp bright.sh /etc/brightSync
+# sudo cp bright.sh /etc/brightSync
 sudo cp bright.service /etc/systemd/system/
 
 success "Installation Complete"
